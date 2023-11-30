b52509e285648a03238c5d789496db95ec72aa97
shortCV <82280581+shortCV@users.noreply.github.com>
2023-11-30 17:29:22 -0500

    refractor image cover

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
+
+//Navigation
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import {NavDropdown} from "react-bootstrap";
+
+//icons
+import Radio from './icons/radio-solid.svg'
 
 
 class App extends Component {
@@ -31,7 +35,10 @@ class App extends Component {
           <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
               <Navbar.Brand href="#home">
-                <img src="icons/radio-solid.svg" width="30" height="30" alt=""  className="d-inline-block align-top"/>{' '}
+                {/*icon: https://fontawesome.com/icons/radio?f=classic&s=solid
+                display: https://blog.logrocket.com/how-to-use-svgs-react/#using-img-tag
+                how to use comments in render: https://wesbos.com/react-jsx-comments*/}
+                <img src={Radio} width="30" height="30" alt=""  className="d-inline-block align-top"/>{' '}
                 Thump
               </Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -43,11 +50,11 @@ class App extends Component {
                   <Nav.Link href="#link">Songs</Nav.Link>
 
                   <NavDropdown title="Playlists" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">My Playlists</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Saved Playlists</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Featured Playlists</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated linkm,nb </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">Create Playlist</NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
                 <Nav className="justify-content-end">
@@ -57,11 +64,13 @@ class App extends Component {
               </Navbar.Collapse>
             </Container>
           </Navbar>
+          <div>
+
+          </div>
           <article>
             <h1>Hello World!</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper velit sed ullamcorper morbi. Viverra aliquet eget sit amet. Eget dolor morbi non arcu. Auctor elit sed vulputate mi sit amet. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Sit amet volutpat consequat mauris nunc congue nisi. Ullamcorper eget nulla facilisi etiam dignissim. Massa enim nec dui nunc mattis enim ut tellus elementum. Vel pretium lectus quam id.</p>
             <button type="button" className="btn btn-outline-success">Comment</button>
-
           </article>
         </section>
     )
