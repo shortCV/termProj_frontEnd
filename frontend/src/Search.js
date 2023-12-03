aad6dc30177aae042984c3c6ec25b2cd1d11e091
shortCV <82280581+shortCV@users.noreply.github.com>
2023-12-03 17:22:15 -0500

    search ability for songs, artists, and albums done

--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -7,6 +7,8 @@ const Search = () => {
     const location = useLocation();
     const searchQuery = new URLSearchParams(location.search).get("q");
     const [filteredSongs, setFilteredSongs] = useState([]);
+    const [filteredArtists, setFilteredArtists] = useState([]);
+    const [filteredAlbums, setFilteredAlbums] = useState([]);
 
     useEffect(() => {
         // Fetch songs based on the search query
@@ -14,11 +16,36 @@ const Search = () => {
             try {
                 const response = await fetch(`http://127.0.0.1:8000/api/get_songs/?search_update=${searchQuery}`);
                 const data = await response.json();
+                console.log(data);
                 setFilteredSongs(data.songs);
             } catch (error) {
                 console.error("Something has gone wrong with filtering songs:", error);
             }
         };
+        // Fetch artists based on the search query
+        const fetchArtists = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/get_artist/?search_update=${searchQuery}`);
+                const data = await response.json();
+                console.log(data);
+                setFilteredArtists(data.artists);
+            } catch (error) {
+                console.error("Something has gone wrong with filtering artists:", error);
+            }
+        };
+        // Fetch albums based on the search query
+        const fetchAlbums = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/get_albums/?search_update=${searchQuery}`);
+                const data = await response.json();
+                console.log(data);
+                setFilteredAlbums(data.albums);
+            } catch (error) {
+                console.error("Something has gone wrong with filtering songs:", error);
+            }
+        };
+        fetchAlbums();
+        fetchArtists();
         fetchSongs();
     }, [searchQuery]);
 
@@ -34,13 +61,18 @@ const Search = () => {
                     <li key={song.title}>{song.title} by {song.artist.join(', ')}</li>
                 ))}
             </ul>
-            {/*            <ul>
-                {filteredSongs.map((song) => (
-                    <li key={song.title}>{song.title} by {song.artist.join(', ')}</li>
-                ))}
-            </ul>*/}
             <h4>Artists</h4>
-            {/* Your playlists component content goes here */}
+            <ul>
+                {filteredArtists.filter(artist => artist.name.includes(searchQuery)).map((artist) => (
+                    <li key={artist.name}>{artist.name}</li>
+                ))}
+            </ul>
+            <h4>Albums</h4>
+            <ul>
+                {filteredAlbums.filter(album => album.title.includes(searchQuery)).map((album) => (
+                    <li key={album.title}>{album.title}</li>
+                ))}
+            </ul>
         </div>
     );
 };
